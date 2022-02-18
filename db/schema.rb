9a3a83c82069ccf9ad7d967c85044c065811619d
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-02-18 11:43:37 +0100

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 2022_02_17_110716) do
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
-    t.string "comment"
-    t.bigint "movie_id", null: false
+    t.text "comment"
     t.bigint "list_id", null: false
+    t.bigint "movie_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
